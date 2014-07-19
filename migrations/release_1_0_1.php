57ef3d1fb3325eb940dfa900f885e43e40333291
Stanislav Atanasov <lucifer@anavaro.com>
2014-07-19 23:20:16 +0300

    Initial 1.0.1 test phase release

---
+++ migrations/release_1_0_1.php
@@ -0,0 +1,41 @@
+<?php
+/**
+*
+* @package migration
+* @copyright (c) 2012 phpBB Group
+* @license http://opensource.org/licenses/gpl-license.php GNU Public License v2
+*
+*/
+
+namespace anavaro\zebraenhance\migrations;
+
+class release_1_0_1 extends \phpbb\db\migration\migration
+{
+	static public function depends_on()
+	{
+		return array('\anavaro\zebraenhance\migrations\release_1_0_0');
+	}
+
+	//lets create the needed table	
+	public function update_schema()
+	{
+		return array(
+			'add_columns'	=> array(
+				$this->table_prefix . 'users_custom'        => array(
+					'zebra_changed'    => array('UINT', 0),
+				)
+			)
+		);
+	}
+
+	public function revert_schema()
+	{
+		return array(
+			'drop_columns'	=> array(
+				$this->table_prefix . 'users_custom'        => array(
+					'zebra_changed',
+				)
+			)
+		);
+	}
+}
