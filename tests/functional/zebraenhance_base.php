a4e83437ec34f03d31161148b81d87664d79c71a
Stanislav Atanasov <lucifer@anavaro.com>
2014-08-21 05:34:53 +0300

    Adding tests

---
+++ tests/functional/zebraenhance_base.php
@@ -0,0 +1,27 @@
+<?php
+/**
+*
+* Zebra enhance 
+*
+* @copyright (c) 2014 Stanislav Atanasov
+* @license GNU General Public License, version 2 (GPL-2.0)
+*
+*/
+
+namespace anavaro\zebraenhance\tests\functional;
+
+/**
+* @group functional
+*/
+class zebraenhance_base extends \phpbb_functional_test_case
+{
+	static protected function setup_extensions()
+	{
+		return array('anavaro/zebraenhance');
+	}
+
+	public function setUp()
+	{
+		parent::setUp();
+	}
+}
