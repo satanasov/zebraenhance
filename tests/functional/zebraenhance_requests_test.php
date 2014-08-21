b2190727d42f527b4703b6fa46dd2dbdf5452b1c
Stanislav Atanasov <lucifer@anavaro.com>
2014-08-21 20:00:31 +0300

    tests

--- tests/functional/zebraenhance_requests_test.php
+++ tests/functional/zebraenhance_requests_test.php
@@ -24,7 +24,7 @@ class zebraenhance_requests_test extends zebraenhance_base
 		$this->login();
 		$crawler = self::request('GET', "ucp.php?i=zebra&add=testuser&sid={$this->sid}");
 		
-		$form = $crawler->selectButton('submit');
+		$form = $crawler->filter('input')->filter('Yes')->selectButton();
 		$crawler = $client->click($form);
 		
 		
